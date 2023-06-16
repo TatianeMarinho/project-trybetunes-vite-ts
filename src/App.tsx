d342957ffc6ffb221de66fff34d73b7dea70eb4a
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-16 18:52:28 -0400

    enviando a primeira

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+
 function App() {
   return (
-    <p>Trybetuness</p>
+    <main>
+      <p>Trybetunes</p>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
+        {/* <Route path="/album/:id" element={ <Album /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/profile/edit" element={ <PorfileEdit /> } />
+        <Route path="*" element={ <NotFound /> } /> */}
+      </Routes>
+    </main>
   );
 }
 
