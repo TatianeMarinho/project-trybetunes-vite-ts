713533707e355baa3b0050f4756087b61ffb487c
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-17 17:21:45 -0400

    conseguiiii a dois

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
+import Album from './pages/Album';
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={ <Login /> } />
         <Route path="/search" element={ <Search /> } />
-        {/* <Route path="/album/:id" element={ <Album /> } />
-        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+        {/* <Route path="/favorites" element={ <Favorites /> } />
         <Route path="/profile" element={ <Profile /> } />
         <Route path="/profile/edit" element={ <PorfileEdit /> } />
         <Route path="*" element={ <NotFound /> } /> */}
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,90 @@
+import { useState } from 'react';
+import searchAlbumsAPI from '../../services/searchAlbumsAPI';
+import Loading from '../../components/loading';
+import AlbumResults from '../../components/albumResults';
+import { AlbumType } from '../../types';
+
+const artistInicial = {
+  artist: '',
+};
+
 function Search() {
+  const [artistName, setArtistName] = useState(artistInicial);
+  const [disabledBtn, setDisabledBtn] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [albumsList, setAlbumsList] = useState<AlbumType[]>([]);
+  const [messageArtist, setMessageArtist] = useState('Resultado de álbuns de: ');
+
+  const handleArtistInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setArtistName({ ...artistName, [name]: value });
+
+    if (artistName.artist.length >= 1) {
+      setDisabledBtn(false);
+    }
+  };
+
+  const handleSubmitArtist = async () => {
+    try {
+      setLoading(true);
+      const result = await searchAlbumsAPI(artistName.artist);
+      console.log(result);
+      if (result) {
+        setAlbumsList(result);
+        setMessageArtist(`Resultado de álbuns de: ${artistName.artist}`);
+        setLoading(false);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    setArtistName(artistInicial);
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <h1>estou na search</h1>
+    <>
+      <form>
+        <label>
+          <input
+            type="text"
+            name="artist"
+            placeholder="Nome do Artista"
+            value={ artistName.artist }
+            onChange={ handleArtistInput }
+            data-testid="search-artist-input"
+          />
+        </label>
+        <button
+          disabled={ disabledBtn }
+          onClick={ handleSubmitArtist }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
+      </form>
+      <div>
+        {
+          (albumsList.length > 0) ? (
+            <div>
+              <h1>
+                { messageArtist }
+              </h1>
+              {
+                albumsList.map((album) => (
+                  <AlbumResults
+                    albuns={ album }
+                    key={ album.collectionId }
+                  />))
+              }
+            </div>)
+            : <h1>Nenhum álbum foi encontrado</h1>
+        }
+      </div>
+    </>
   );
 }
+
 export default Search;
