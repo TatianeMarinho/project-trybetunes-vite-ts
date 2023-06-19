9e1ba4ebb7b05d91e2a9d0ee9f046b4b6cac0538
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-18 20:23:22 -0400

    enviando a nove

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,6 @@
+function NotFound() {
+  return (
+    <h3>notfound</h3>
+  );
+}
+export default NotFound;
---
+++ src/pages/ProfileEdit/index.tsx
@@ -0,0 +1,6 @@
+function ProfileEdit() {
+  return (
+    <h2>to no profileEdit</h2>
+  );
+}
+export default ProfileEdit;
